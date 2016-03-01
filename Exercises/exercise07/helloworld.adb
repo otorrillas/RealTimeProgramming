4c764ad75c0405a903e3e2d3a1c713b252586806
otorrillas <otorrillas@gmail.com>
2016-03-01 21:53:43 +0100

    Exercise 07: Ada

---
+++ Exercises/exercise07/helloworld.adb
@@ -0,0 +1,10 @@
+-----------------------------------------------------------
+-- Hello World program
+-----------------------------------------------------------
+with Ada.Text_Io;
+use Ada.Text_Io;
+
+procedure HelloWorld is
+begin
+   Put_Line("Hello World!");
+end HelloWorld;
